d9f6f230df9282de6a5ae76eec6fbd27801ede89
andrian-klikcair <andrian@klikcair.com>
2022-08-27 14:40:14 +0700

    feat(infra): add infra layer that responsible to create http server

---
+++ src/Infrastructure/http/createServer.ts
@@ -0,0 +1,19 @@
+import express, { Express } from 'express'
+import cors from 'cors'
+import Container from '../container'
+import { CreateConnection } from '../databases/mongodb/createConnection'
+import { ProductCategoryAPI } from '../../Interface/http/api/productCategories'
+import Middleware from './middlewares'
+require('express-async-errors')
+
+export const createServer = async (container: typeof Container): Promise<Express> => {
+  await CreateConnection()
+  const server = express()
+  const middleware = new Middleware()
+  server.use(cors())
+  server.use(express.json())
+
+  server.use('/product-categories', ProductCategoryAPI(container))
+  server.use(middleware.errorHandler)
+  return server
+}
